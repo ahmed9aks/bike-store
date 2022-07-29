6a97b816a458a942c705e1aa644936abfd9d835f
ahmed9aks <97857810+ahmed9aks@users.noreply.github.com>
2022-07-29 02:13:22 -0400

    1

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,11 +1,11 @@
 <?php
-$serverName= "localhost";
-$dBUsername= "root";
-$dBPassword= "";
-$dBName = "phpbikestore";
+$serverName= 'localhost';
+$dBUsername= 'root';
+$dBPassword= '';
+$dBName = 'phpbikestore';
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
 if(!$conn){
-    die("Connection failed: " . mysqli_connect_error());
+    die('Connection failed: ' . mysqli_connect_error());
 }
